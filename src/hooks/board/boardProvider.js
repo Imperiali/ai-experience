9ff2b81ebaa0952c528c1db7293f81e9ecf638f6
Imperiali <igor.imperiali@gmail.com>
2021-08-30 14:26:08 -0300

    [boardProvider] Refactor initial state

--- src/hooks/board/boardProvider.js
+++ src/hooks/board/boardProvider.js
@@ -1,16 +1,19 @@
 import React, {useMemo, useReducer} from 'react';
 
 export const BoardContext = React.createContext()
+
+const initialState = {
+  history: [{ //histórico de jogadas vários array contento a o valor que foi adicionado no square X ou O
+    squares: Array(9).fill(null),
+  }],
+  stepNumber: 0, //determina em qual jogada o jogo esta
+  xIsNext: true, //determina quem é o próximo a jogar X ou O
+}
+
 export const boardReducer = (state, action) => {
   switch (action.type) {
     case 'RESTART':
-      return {
-        history: [{ //histórico de jogadas vários array contento a o valor que foi adicionado no square X ou O
-          squares: Array(9).fill(null),
-        }],
-        stepNumber: 0, //determina em qual jogada o jogo esta
-        xIsNext: true, //determina quem é o próximo a jogar X ou O
-      }
+      return initialState
     case 'UPDATE_TURN':
       return {
         history: state.history.concat([{
@@ -24,16 +27,6 @@ export const boardReducer = (state, action) => {
   }
 }
 const BoardProvider = ({ children}) => {
-  const initialState = {
-    history: [{ //histórico de jogadas vários array contento a o valor que foi adicionado no square X ou O
-      squares: Array(9).fill(null),
-    }],
-    stepNumber: 0, //determina em qual jogada o jogo esta
-    xIsNext: true, //determina quem é o próximo a jogar X ou O
-  }
-
-  console.log('initialState', initialState)
-
   const [state, dispatch] = useReducer(boardReducer, initialState)
   const contextValue = useMemo(() => ({state, dispatch}), [state, dispatch])
 
