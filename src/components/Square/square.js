e0d0aad2265684d3a64640598be1cc93e097736d
Imperiali <igor.imperiali@gmail.com>
2021-08-30 14:20:43 -0300

    Update square to have his on responsibilities

--- src/components/Square/square.js
+++ src/components/Square/square.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './square.css';
+import {calculateWinner} from "../utils/calculateWinner";
+import {useBoard} from "../../hooks/board";
 
 const Square = (props) => {
-  const { value, onClick } = props;
+  const { squareId } = props;
+
+  const {history, xIsNext, updateTurn, stepNumber} = useBoard()
+
+  const handleClick = () => {
+    const current = history[history.length - 1]; //pega o array corrente do history
+    const squares = current.squares.slice(); //faz uma cópia do objeto que esta no array current
+
+    if (calculateWinner(squares) || squares[squareId]) {
+      return;
+    }
+
+    squares[squareId] = xIsNext ? 'X' : 'O';
+
+    updateTurn(squares)
+  }
+
+  const current = history[stepNumber];
+
   return (
-    <button className="square" onClick={onClick}>
-      {value}
+    <button className="square" onClick={handleClick}>
+      {current.squares[squareId]}
     </button>
   )
 }
