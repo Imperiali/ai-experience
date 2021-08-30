38a66b098f747781befa9c56a11572997b2be913
Imperiali <igor.imperiali@gmail.com>
2021-08-30 19:02:51 -0300

    [Board] Modify component to have more responsibilities

--- src/components/Board/board.js
+++ src/components/Board/board.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Square from '../Square';
 import './board.css';
-import {useBoard} from "../../hooks/board";
+import classnames from "classnames";
+import {useSuperBoard} from "../../hooks/superBoard";
+import {calculateWinner} from "../utils/calculateWinner";
+import Winner from "../Winner";
 
-const Board = () => {
-  // const { boardId } = props;
-  const {winner} = useBoard()
+const Board = (props) => {
+  const { hasBorder = false, boardId } = props
+  const { board, setWinner } = useSuperBoard(boardId)
+
+  const winner = calculateWinner(board.squares)
+
+  useEffect(() => {
+    if (winner) {
+      setWinner()
+    }
+  }, [winner])
 
   return (
     <div className="board-wrapper">
-      { winner && <div className="winner">{winner}</div>}
+      { winner && <Winner winner={winner} />}
       <div className="board-row">
-        <Square squareId={0}/>
-        <Square squareId={1}/>
-        <Square squareId={2}/>
+        <Square squareId={0} boardId={boardId}/>
+        <Square squareId={1} boardId={boardId}/>
+        <Square squareId={2} boardId={boardId}/>
       </div>
       <div className="board-row">
-        <Square squareId={3}/>
-        <Square squareId={4}/>
-        <Square squareId={5}/>
+        <Square squareId={3} boardId={boardId}/>
+        <Square squareId={4} boardId={boardId}/>
+        <Square squareId={5} boardId={boardId}/>
       </div>
-      <div className="board-row">
-        <Square squareId={6}/>
-        <Square squareId={7}/>
-        <Square squareId={8}/>
+      <div className={classnames("board-row", {
+        'board-border': hasBorder
+      })}>
+        <Square squareId={6} boardId={boardId}/>
+        <Square squareId={7} boardId={boardId}/>
+        <Square squareId={8} boardId={boardId}/>
       </div>
     </div>
   );
